c1425e50927fb4c27e4b03c20b24a071c2a7e055
Volkan Özçelik <ovolkan@vmware.com>
2023-12-04 21:36:30 -0800

    add publish script
    Signed-off-by: Volkan Özçelik <ovolkan@vmware.com>

--- hack/publish.sh
+++ hack/publish.sh
@@ -11,18 +11,11 @@
 
 mkdocs build
 
-if [[ -z "$VG_S3_BUCKET" || -z "$VG_DISTRIBUTION_ID" ]]; then
-  echo "Error: $VG_S3_BUCKET and $VG_DISTRIBUTION_ID must be set."
+if [[ -z "$Z2H_DEV_S3_BUCKET" || -z "$Z2H_DEV_DISTRIBUTION_ID" ]]; then
+  echo "Error: $Z2H_DEV_S3_BUCKET and $Z2H_DEV_DISTRIBUTION_ID must be set."
   exit 1
 fi
 
-if [[ -z "$VG_S3_BUCKET_2" || -z "$VG_DISTRIBUTION_ID_2" ]]; then
-  echo "Error: $VG_S3_BUCKET_2 and $VG_DISTRIBUTION_ID_2 must be set."
-  exit 1
-fi
+aws s3 sync site/ "$Z2H_DEV_S3_BUCKET"
 
-aws s3 sync site/ "$VG_S3_BUCKET"
-aws s3 sync site/ "$VG_S3_BUCKET_2"
-
-aws cloudfront create-invalidation --distribution-id "$VG_DISTRIBUTION_ID" --paths "/*"
-aws cloudfront create-invalidation --distribution-id "$VG_DISTRIBUTION_ID_2" --paths "/*"
+aws cloudfront create-invalidation --distribution-id "$Z2H_DEV_DISTRIBUTION_ID" --paths "/*"
